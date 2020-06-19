615ccc5cf3902fef7b438ce56a2629d37688e489
hablijack <christoph.habel@posteo.de>
2020-06-19 13:18:52 +0200

    weather parser in one class

---
+++ app/intents/Weather.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import logging
+from library.Persistence import Persistence
+
+
+def handle(text):
+    logger = logging.getLogger('WeatherModule')
+    logger.info("... executing weather module")
+    forecast = Persistence.read("wetteronline")
+    answer = ""
+    if "heute" in text:
+        answer = forecast['today']
+    elif "morgen" in text:
+        answer = forecast['tomorrow']
+    return answer
+
+def is_requested(intent):
+    return (intent == "Weather")
