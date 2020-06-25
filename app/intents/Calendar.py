fa8c23ad1c4a646e122f38d9f4d3680039562be8
hablijack <christoph.habel@posteo.de>
2020-06-25 15:50:41 +0200

    trashhandling added

--- app/intents/Calendar.py
+++ app/intents/Calendar.py
@@ -12,19 +12,20 @@ def handle(text):
     agenda = ""
     for event in calendar['events']:
         user_names = []
+        trashinfo = False
         TEAMUP_USERS = [
             {"name": "Christoph", "calendar_id": 4377398},
             {"name": "Barbara", "calendar_id": 4377397},
             {"name": "Family", "calendar_id": 4377538},
             {"name": "Feiertage", "calendar_id": 4377396},
-            {"name": "Geburtstage", "calendar_id": 4377496}
+            {"name": "Geburtstage", "calendar_id": 4377496},
+            {"name": "Müll", "calendar_id": 7539853}
         ]
         for calendar_id in event["subcalendar_ids"]:
             for user in TEAMUP_USERS:
                 if calendar_id == user["calendar_id"]:
                     user_names.append(user["name"])
                     break
- 
         if user_names[0] == "Family":
             user_names[0] = "Barbara"
             user_names.append("Christoph")
@@ -33,12 +34,18 @@ def handle(text):
             user_string += " haben heute "
         else:
             user_string += " hat heute "
-        if event["all_day"]:
-            agenda += user_string + "den ganzen Tag " + event["title"] + ". "
+            if user_names[0] == "Müll":
+                trashinfo = True
+        if trashinfo:
+            agenda += "Achtung, heute ist: " + event["title"] + "! "
         else:
-            time_string = event["start_dt"]
-            event_time = datetime.datetime.strptime(time_string[:len(time_string)-3] + time_string[len(time_string)-2:], '%Y-%m-%dT%H:%M:%S%z')
-            agenda += user_string + "um " + "{0:%H:%M}".format(event_time) + "Uhr " + event['title'] + ". "
+            if event["all_day"]:
+                agenda += user_string + "den ganzen Tag " + event["title"] + ". "
+            else:
+                time_string = event["start_dt"]
+                event_time = datetime.datetime.strptime(time_string[:len(time_string)-3] + time_string[len(time_string)-2:], '%Y-%m-%dT%H:%M:%S%z')
+                agenda += user_string + "um " + "{0:%H:%M}".format(event_time) + "Uhr " + event['title'] + ". "
+        
     if len(calendar['events']) == 0:
         agenda = "Du hast heute keine Termine mehr."
     return agenda
