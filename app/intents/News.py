7eb75f2b50b2ec0c5f38e22082ab27b295b2602e
hablijack <christoph.habel@posteo.de>
2020-06-30 18:43:01 +0200

    quickfix

--- app/intents/News.py
+++ app/intents/News.py
@@ -11,7 +11,7 @@ def handle(text):
     logger = logging.getLogger('NewsModule')
     logger.info("... executing News module")
     url = Persistence.read("tagesschau")
-    requests.post("192.168.178.52:5000", json={"topic": "news", "url": url})
+    requests.post("http://192.168.178.52:5000/change", json={"topic": "news", "url": url})
     return "Hier ist die aktuelle ARD Tagesschau."
 
 def is_requested(intent):
