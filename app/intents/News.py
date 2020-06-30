25223f7ef2bed6008401cbce1cd8584b523e128e
hablijack <christoph.habel@posteo.de>
2020-06-30 18:10:26 +0200

    News Docker Module

--- app/intents/News.py
+++ app/intents/News.py
@@ -3,12 +3,16 @@
 
 import logging
 from library.Persistence import Persistence
+import requests
+import json
 
 
 def handle(text):
     logger = logging.getLogger('NewsModule')
     logger.info("... executing News module")
-    return Persistence.read("tagesschau")
+    url = Persistence.read("tagesschau")
+    requests.post("192.168.178.52:5000", json={"topic": "news", "url": url})
+    return "Hier ist die aktuelle ARD Tagesschau."
 
 def is_requested(intent):
     return ("Tagesschau" == intent)
