b7e419cdf353234f9abed7c8f896d169e92ba682
hablijack <christoph.habel@posteo.de>
2020-07-01 08:39:36 +0200

    cleanup

--- app/intents/News.py
+++ app/intents/News.py
@@ -10,8 +10,8 @@ import json
 def handle(text):
     logger = logging.getLogger('NewsModule')
     logger.info("... executing News module")
-    url = Persistence.read("tagesschau")
-    requests.post("http://192.168.178.52:5000/change", json={"topic": "news", "url": url})
+    tagesschau = Persistence.read("tagesschau")
+    requests.post("http://192.168.178.52:5000/change", json={"topic": "news", "url": tagesschau['url']})
     return "Hier ist die aktuelle ARD Tagesschau."
 
 def is_requested(intent):
