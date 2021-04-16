1c49801b3f6a5d96524489e918a6db06d15044a6
hablijack <christoph.habel@posteo.de>
2021-04-16 13:15:45 +0200

    Intent added

---
+++ app/intents/Corona.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import logging
+from library.Persistence import Persistence
+
+
+def handle(text, slots, scheduler):
+    logger = logging.getLogger('CoronaIntent')
+    logger.info("... executing Corona intent")
+    incident = Persistence.read("corona")
+    response = "Der aktuelle 7 Tage Inzidenzwert pro 100000 Einwohner im Landkreis Tirschenreuth beträgt: " + incident
+    return response
+
+
+def is_requested(intent):
+    return ("Corona" == intent)
